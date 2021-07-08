b951ad269e4667e94403ca85b89cdcaa48c9ea26
Shahriar Parvez <mr.spshuvo@gmail.com>
2021-07-08 09:47:36 +0600

    :star2: add hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Hero = () => (
+  <section class="hero">
+    <div className="container">
+      <h2 class="heading">Find your ideal home</h2>
+      <p class="heading heading--sub">
+        Search from more than 19 million of inspected appartments, houses, cottages, villas, manors
+        and mansions
+      </p>
+    </div>
+  </section>
+);
+
+export default Hero;

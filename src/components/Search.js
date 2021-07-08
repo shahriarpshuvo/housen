391f4410438891ae462e5a342127f521357e42b7
Shahriar Parvez <mr.spshuvo@gmail.com>
2021-07-08 13:21:20 +0600

    :fire: add search list

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { ReactComponent as SearchIcon } from '../assets/icons/search.svg';
+import { ReactComponent as FilterIcon } from '../assets/icons/filter.svg';
+import { ReactComponent as ChevronRightIcon } from '../assets/icons/chevron-right.svg';
 
 const Search = () => {
   return (
@@ -12,6 +14,24 @@ const Search = () => {
           <input type="text" id="searchText" placeholder="Search for address" />
           <button class="btn btn--main">Search</button>
         </form>
+        <div className="search__list">
+          <div className="search__item">
+            <h3 className="search__itemCity">Calabasas, CA</h3>
+            <div className="search__itemInfo meta">
+              <FilterIcon />
+              <span>$1.5k+ / 2 Beds / Appt. ... 3 more</span>
+              <ChevronRightIcon />
+            </div>
+          </div>
+          <div className="search__item">
+            <h3 className="search__itemCity">Calabasas, CA</h3>
+            <div className="search__itemInfo meta">
+              <FilterIcon />
+              <span>$1.5k+ / 2 Beds / Appt. ... 3 more</span>
+              <ChevronRightIcon />
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
